27946850566f12e7bc4b5e360668254ad3ffa397
Adam Fridén Rasmussen <113016730+AdamFR00@users.noreply.github.com>
2026-09-15 21:35:36 +0200

    First version (#15)
    Added the corresponding CRUD logic to the quotes page. Did some minor
    styling changes to books page.

--- frontend/book-app/src/app/components/createbook/createbook.ts
+++ frontend/book-app/src/app/components/createbook/createbook.ts
@@ -3,16 +3,19 @@ import { HttpClient } from '@angular/common/http';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { environment } from '../../../environments/environment';
 import { Router, RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FontAwesomeModule, RouterLink],
   selector: 'app-createbook',
   styleUrl: './createbook.css',
   templateUrl: './createbook.html',
 })
-export class Createbook {
+export class CreateBook {
   private http = inject(HttpClient);
   private router = inject(Router);
+  faArrowLeft = faArrowLeft;
   bookForm: FormGroup = new FormGroup({
     Title: new FormControl<string>('', {
       nonNullable: true,
@@ -31,7 +34,6 @@ createBook() {
   this.http.post(`${environment.apiUrl}/books`, this.bookForm.value, {withCredentials: true})
   .subscribe({
     next: (response) => {
-      debugger;
       console.log("Created book!", response);
       this.router.navigate(['/home/books']);
     },
---
+++ frontend/book-app/src/app/models/quote.ts
@@ -0,0 +1,4 @@
+export interface Quote {
+    quoteId: number;
+    content: string;
+}
